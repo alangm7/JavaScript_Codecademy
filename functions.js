26181bddc25b72b100124a465ade66753f24d6be
Alan García Mendoza <alangm7@outlook.com>
2016-09-29 16:27:39 -0500

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,7 @@
+var makePizza = function (topping, crustType) {
+  return 'The ' + crustType + ' crust pizza topped with ' + topping + ' is done. Let\'s eat!';
+};
+
+var customPizza = makePizza("bacon","pinapple");
+
+console.log(customPizza);
