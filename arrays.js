bc2578d49ca2b04d30645b7606eeeb5f72235123
Alan García Mendoza <alangm7@outlook.com>
2016-09-30 13:35:50 -0500

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var bucketList = ['apple','carrot','cherry'];
+bucketList.push('onion','peach'); //push, add element in array//
+bucketList.pop(); //pop, delete last element in array//
+console.log(bucketList.length);
+console.log(bucketList);
