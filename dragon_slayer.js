79e1a7bfc4a1f427c0babb1d6acccb3cb4fd3cc1
Alan García Mendoza <alangm7@outlook.com>
2017-04-10 13:57:37 -0500

    Create dragon_slayer.js

---
+++ dragon_slayer.js
@@ -0,0 +1,22 @@
+var slaying = true;
+var youHit = (Math.floor() * 2);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = 0;
+
+while(slaying) {
+    if(youHit) {
+        console.log("You kill the dragon!");
+        totalDamage += damageThisRound;
+            if(totalDamage >= 4){
+                console.log("You win!");
+                slaying = false;
+            } else {
+                youHit = Math.floor(Math.random() * 2);
+            }
+    } else {
+        console.log("The dragon kill you, you lose!");
+        slaying = false;
+    }
+}
+
+
