dfec061b83762dda60174827ec0026c3dfd12613
Alan García Mendoza <alangm7@outlook.com>
2017-04-10 13:17:26 -0500

    Create forWhileDo.js

---
+++ forWhileDo.js
@@ -0,0 +1,18 @@
+//To finish up and prove your loop mastery,
+write three syntactically correct loops in the editor:
+one for, one while, and one do. Beware of infinite loops!
+
+for (var i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+var condition = true;
+while(condition) {
+    console.log("Hello!");
+    condition = false;
+}
+
+var condition = false;
+do {
+    console.log("I'm printed once!");
+} while(condition);
